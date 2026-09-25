3f103887abc134a9d1a1f2a5bda40bb9ed48e2a4
Danielle <dgolinsky@thunderbird.net>
2026-09-25 12:32:19 -0400

    test(donation-appeal): set the minimum time in the app to 1s, and delay after that to 5s, to allow for testers to check both the delay and that it displays

--- feature/funding/common/src/main/kotlin/net/thunderbird/feature/funding/common/api/FundingReminderContract.kt
+++ feature/funding/common/src/main/kotlin/net/thunderbird/feature/funding/common/api/FundingReminderContract.kt
@@ -4,10 +4,10 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 
 // 1 week in milliseconds
-const val FUNDING_REMINDER_DELAY_MILLIS = 7 * 24 * 60 * 60 * 1000L
+const val FUNDING_REMINDER_DELAY_MILLIS = 5000L //7 * 24 * 60 * 60 * 1000L
 
 // 30 minutes in milliseconds
-const val FUNDING_REMINDER_MIN_ACTIVITY_MILLIS = 30 * 60 * 1000L
+const val FUNDING_REMINDER_MIN_ACTIVITY_MILLIS = 1000 //30 * 60 * 1000L
 
 interface FundingReminderContract {
 
